bcabc532dbdb664fc89e7626055e5cdf5009e509
Heddi Islam <islamheddi19@gmail.com>
2026-06-05 12:50:22 +0100

    server: add the message socket

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,22 +1,36 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "http"
 import { app } from "./index.js"
-import { WebSocketServer } from "ws"
+import socketIO from "socket.io"
+import { createMessage } from "./controller/messages.controller.js"
+import { MessageType } from "./types/message.types.js"
 
 const server: Server<typeof IncomingMessage, typeof ServerResponse> = createServer(app)
-const wss: WebSocketServer = new WebSocketServer({server});
 
-wss.on('connection', (ws) => {
-  console.log('Client connected via WebSocket');
+const SocketMap = new Map<string, string>();// Map to store WebSocket connections with their IDs
 
-  // Handle incoming messages from the client
-  ws.on('message', (message) => {
-    console.log(`Received: ${message}`);
-    // Echo the message back to the client
-    ws.send(`Server received: ${message}`);
+const io = new socketIO.Server(server, {
+  cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+  }
+})
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query?.userId;
+  SocketMap.set(userId as string, socket.id)
+
+  socket.on("message", async (msg: MessageType) => {
+    const reply = await createMessage(msg);
+    const socketId = SocketMap.get(msg.userId);
+    socket.to(socketId as string).emit("receive-message", reply)
+  })
+
+  socket.on("disconnecting", () => {
+    for (const [userId, socketId] of SocketMap.entries()) {
+      if (socketId === socket.id) {
+        SocketMap.delete(userId);
+        break;
+      }
+    }
   });
-
-  // Handle client disconnection
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
-});
+})
