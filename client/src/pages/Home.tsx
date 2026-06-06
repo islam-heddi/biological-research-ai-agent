6804dfdfa908a5554c75530dd34a77e116929978
Heddi Islam <islamheddi19@gmail.com>
2026-06-06 10:37:24 +0100

    client: init

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (<>
+    <div>
+        <h1 className="text-6xl md:text-8xl font-black mb-6 leading-none tracking-tight">The <span className="text-[#00ff41]">Cybernetic</span><br /><span className="text-[#00d4ff]">Immune System</span><br />for High-Frequency Software</h1>
+        <p className="font-[ &quot;JetBrains Mono&quot;, monospace; opacity: 1;] text-lg md:text-xl text-gray-400 mb-12 max-w-3xl mx-auto leading-relaxed">Replace legacy mutation testing with <span className="text-[#00ff41]">Algebraic Topology</span> and <span className="text-[#00d4ff]">Mechanical Sympathy</span>. Achieve deterministic code immunity at kernel-level precision.</p>
+    </div>
+    </>
+  )
+}
