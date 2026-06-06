c92295cf6a9b13885576e4c899aa66e82466384c
Heddi Islam <islamheddi19@gmail.com>
2026-06-06 12:17:06 +0100

    client: add Flash button

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,19 @@
+import Button from "../Components/FlashButton"
 import NoctureCard from "../Components/NoctureCard"
 
 export default function Home() {
   return (<>
     <div>
-        <div className="absolute inset-0 z-0" style={{ backgroundImage: "linear-gradient(rgb(47, 47, 47) 1px, transparent 1px), linear-gradient(90deg, rgb(47, 47, 47) 1px, transparent 1px)", backgroundSize: "50px 50px" }}></div>
-        <div className="flex flex-col items-center relative z-10">
-            <h1 className="text-center text-6xl md:text-8xl font-black mb-6 leading-none tracking-tight">The <span className="text-[#00ff41]">Cybernetic</span><br /><span className="text-[#00d4ff]">Immune System</span><br />for High-Frequency Software</h1>
-            <p className="text-center font-['JetBrains Mono',monospace] text-lg md:text-xl text-gray-400 mb-12 max-w-3xl mx-auto leading-relaxed">Replace legacy mutation testing with <span className="text-[#00ff41]">Algebraic Topology</span> and <span className="text-[#00d4ff]">Mechanical Sympathy</span>. Achieve deterministic code immunity at kernel-level precision.</p>
-        </div>
-        <NoctureCard />
-        <div>
+      <div className="absolute inset-0 z-0" style={{ backgroundImage: "linear-gradient(rgb(47, 47, 47) 1px, transparent 1px), linear-gradient(90deg, rgb(47, 47, 47) 1px, transparent 1px)", backgroundSize: "50px 50px" }}></div>
+      <div className="flex flex-col items-center relative z-10">
+          <h1 className="text-center text-6xl md:text-8xl font-black mb-6 leading-none tracking-tight"><span className="text-[#00ff41]">biology</span><br /><span className="text-[#00d4ff]">research AI agent System</span><br />for Best research.</h1>
+          <p className="text-center font-['JetBrains Mono',monospace] text-lg md:text-xl text-gray-400 mb-12 max-w-3xl mx-auto leading-relaxed">Check the latest trends of <span className="text-[#00ff41]">Biology</span> and <span className="text-[#00d4ff]">ask questions with AI</span>. Achieve high level of understanding.</p>
+          <Button className="p-4">Get Started</Button>
+      </div>
+      <NoctureCard />
+      <div>
 
-        </div>
+      </div>
     </div>
     </>
   )
