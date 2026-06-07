80457ee8c4b6b6a130bd602de20268aad1166142
Heddi Islam <islamheddi19@gmail.com>
2026-06-07 15:18:30 +0100

    client: add dashboard route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import { ToastContainer } from "react-toastify"
+import Dashboard from "./pages/Dashboard"
 function App() {
   return (<>
   <ToastContainer
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
     </>
