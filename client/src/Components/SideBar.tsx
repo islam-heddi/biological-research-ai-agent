7e3cf40d6e2ad0d55d0bd2c15419e2a1953762c2
Heddi Islam <islamheddi19@gmail.com>
2026-06-08 13:11:49 +0100

    client: add items on sidebar

--- client/src/Components/SideBar.tsx
+++ client/src/Components/SideBar.tsx
@@ -1,21 +1,50 @@
 import { useSelector } from "react-redux"
-function SideBar() {
+import { Link } from "react-router-dom";
+import { Fragment } from "react/jsx-runtime";
+import { CircleUser, Leaf, Settings } from "lucide-react";
+import { Cpu } from "lucide-react";
+import { Dna } from "lucide-react";
+
+type NavigationType = {
+    name: string;
+    link: string;
+}
+
+interface ISideBar {
+    list: NavigationType[]
+}
+function SideBar({list}: ISideBar) {
     useSelector((state: any)=> console.log(state))
     const user = useSelector((state: any)=> state.auth.value.user)
+    const selectIcon = (title: string) => {
+        if(title == "Setting") {
+            return <Settings color="#11ff00" />
+        }else if(title == "Researchs") {
+            return <Dna color="#11ff00" />
+        }else if(title == "Chatbot") {
+            return <Cpu color="#11ff00" />
+        }else{
+            return <></>
+        }
+    }
   return (
-    <div>
-        <div>
-            {user}
+    <div className="inline-flex flex-col bg-[#1f1f1f] h-screen rounded-2xl w-3/12 min-w-52 max-w-75">
+        <div className="flex flex-row justify-center">
+      <Leaf size={"50px"} color="#11ff00" />
+      <h1 className="text-2xl m-3">ChatBio</h1>
+      </div>
+        <div className="p-4 flex flex-row gap-2 items-center">
+            <CircleUser size={"50px"} color="#11ff00" />
+            <p className="text-lg">{user}</p>
         </div>
-        <div>
-            Chat bot
-        </div>
-        <div>
-            Researchs
-        </div>
-        <div>
-            Settings
-        </div>
+        {list.map((value, index) => <Fragment key={index}>
+            <Link className="text-white hover:bg-gray-600 p-4 inline-flex flex-row gap-2" to={value.link}>
+                <div className="p-4 inline-flex flex-row gap-2">
+                    {selectIcon(value.name)}
+                    {value.name}
+                </div>
+            </Link>
+        </Fragment>)}
     </div>
   )
 }
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -3,7 +3,7 @@ import SideBar from "../Components/SideBar"
 function Dashboard() {
   return (
     <div>
-      <SideBar />
+      <SideBar list={[{name: "Chatbot", link: "/chat"}, {name: "Researchs", link: "/Researchs"}, {name: "Setting", link:"/settings"}]}/>
     </div>
   )
 }
