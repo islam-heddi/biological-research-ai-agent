71c01d1b8039ee68bed22a19c09dbebd79efab83
Heddi Islam <islamheddi19@gmail.com>
2026-06-08 13:26:14 +0100

    client: add logout button

--- client/src/Components/SideBar.tsx
+++ client/src/Components/SideBar.tsx
@@ -1,9 +1,12 @@
 import { useSelector } from "react-redux"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 import { CircleUser, Leaf, Settings } from "lucide-react";
 import { Cpu } from "lucide-react";
 import { Dna } from "lucide-react";
+import { toast } from "react-toastify";
+import { api } from "../api/api";
+import { DECONNECT } from "../api/endpoints.constants";
 
 type NavigationType = {
     name: string;
@@ -14,8 +17,18 @@ interface ISideBar {
     list: NavigationType[]
 }
 function SideBar({list}: ISideBar) {
-    useSelector((state: any)=> console.log(state))
+    const navigate = useNavigate()
     const user = useSelector((state: any)=> state.auth.value.user)
+    const deconnect = async () => {
+        try {
+            await api.delete(DECONNECT)
+            navigate("/login")
+            toast.success("Log out successfully");
+        } catch (error) {
+            toast.error("Error when logout")
+        }
+    }
+
     const selectIcon = (title: string) => {
         if(title == "Setting") {
             return <Settings color="#11ff00" />
@@ -29,7 +42,7 @@ function SideBar({list}: ISideBar) {
     }
   return (
     <div className="inline-flex flex-col bg-[#1f1f1f] h-screen rounded-2xl w-3/12 min-w-52 max-w-75">
-        <div className="flex flex-row justify-center">
+        <div className="flex flex-row justify-center p-4">
       <Leaf size={"50px"} color="#11ff00" />
       <h1 className="text-2xl m-3">ChatBio</h1>
       </div>
@@ -45,6 +58,14 @@ function SideBar({list}: ISideBar) {
                 </div>
             </Link>
         </Fragment>)}
+        <div className="cursor-pointer" >
+        <div onClick={() => deconnect()} className="bottom-0 absolute text-white hover:bg-gray-600 p-4 inline-flex flex-row gap-2">
+            <div className="p-4 inline-flex flex-row gap-2">
+                <CircleUser color="#11ff00" />
+                Logout
+            </div>
+        </div>
+        </div>
     </div>
   )
 }
