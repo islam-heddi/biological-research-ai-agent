5be3fb3f40dcca3793bf86b7c1fd4664e1005ec9
Heddi Islam <islamheddi19@gmail.com>
2026-06-12 19:08:36 +0100

    client: add skelton while thinking

--- client/src/Components/Messages.tsx
+++ client/src/Components/Messages.tsx
@@ -3,14 +3,17 @@ import type { MessageType } from "../types/types"
 import {marked} from "marked"
 import parse from "html-react-parser"
 import { useSelector } from "react-redux"
+import Skeleton from "react-loading-skeleton"
 
 
 function Messages() {
     const messages: MessageType[] = useSelector((state: any) => state.chat.value)
+    const isThinking = useSelector((state: any) => state.think.value);
   return (
     <div className=" flex flex-col">
       {messages.length < 1? "Start a message": ""}
         {messages.map((value,index) => <React.Fragment key={index}><div className={`${value.role === "user" ? "self-end bg-[#0f24]" : "self-start"} m-2 rounded-2xl  p-7 inline`}>{value.role==="system"?parse((marked(value.content) as string).replaceAll("*", "<br />")): value.content}</div></React.Fragment>)}   
+      {isThinking? <Skeleton baseColor='#0d0d0d5b' count={5} enableAnimation={true}/>: <></>}   
     </div>
   )
 }
